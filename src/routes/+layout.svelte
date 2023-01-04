5da7ff7c50bba0d3a35597693280ee471aa38453
Octavio Lara <octavioasapchi@gmail.com>
2023-01-04 01:12:34 -0400

    [:tada: initialize] Svelte/Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Navbar from './components/navbar.svelte';
+</script>
+
+<div>
+	<Navbar />
+	<div class="my-16 relative">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/components/navbar.svelte
@@ -0,0 +1,5 @@
+<div
+	class="shadow-md h-50 w-full text-white bg-emerald-400 h-14 flex items-center px-10 py-5 fixed top-0"
+>
+	<h2 class="font-bold text-lg">Rick and morty</h2>
+</div>
